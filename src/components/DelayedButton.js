0245aa71930f2df81bd2d9848d45d499bd166898
AidanMcB <aidankmcbride@gmail.com>
2020-05-14 16:39:35 -0500

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,28 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+export default class DelayedButton extends React.Component{
+
+
+    // Event Pooling:
+    //   the properties of the event only exist while 
+    //   the callback is active. Adding async to the mix, 
+    //   or storing the event for future use, will fail.
+    handleClick = (event) => {
+        //remove the synthetic event form the pool
+        //and allow references to the event to be
+        //made by the user code
+        
+        event.persist()
+        //this.props.delay = setTimeout()
+        window.setTimeout(() => {
+            this.props.onDelayedClick(event)
+        }, this.props.delay)
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}></button>
+        )
+    }
+}
